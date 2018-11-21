489a927fd9f7bc55ed077c3d385e7362bb46d2e9
646547989 <646547989@qq.com>
2018-11-21 17:41:53 +0800

    创建帖子分类

---
+++ database/migrations/2018_11_21_172152_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50)->index()->comment('分类名');
+            $table->string('description')->nullable()->comment('分类描述');
+            $table->integer('post_sums')->default(0)->comment('帖子总数');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
