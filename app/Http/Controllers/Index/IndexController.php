60de936e8f878340c09e6a8f8cb905bf0f11b1d0
646547989 <646547989@qq.com>
2018-11-30 17:26:59 +0800

    增加外键，防止数据损坏

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -3,10 +3,12 @@ namespace App\Http\Controllers\Index;
 
 
 use Faker\Generator;
+use Illuminate\Support\Facades\Cache;
 
 class IndexController extends BaseController
 {
     public function index(Generator $faker){
+        dd(Cache::flush());
         return view('index.index.index');
     }
 }
--- database/migrations/2018_11_29_104706_seed_roles_and_permissions_data.php
+++ database/migrations/2018_11_29_104706_seed_roles_and_permissions_data.php
@@ -21,19 +21,57 @@ class SeedRolesAndPermissionsData extends Migration
         app()['cache']->forget('spatie.permission.cache');
 
         // 先创建权限
-        Permission::create(['name' => 'manage_contents']);
-        Permission::create(['name' => 'manage_users']);
-        Permission::create(['name' => 'edit_settings']);
+        Permission::create(['name' => 'post_del', 'title' => '删除帖子']);
+        Permission::create(['name' => 'post_index', 'title' => '查看帖子']);
+        Permission::create(['name' => 'reply_del', 'title' => '删除回复']);
+        Permission::create(['name' => 'reply_index', 'title' => '查看回复']);
+        Permission::create(['name' => 'user_add', 'title' => '添加用户']);
+        Permission::create(['name' => 'user_del', 'title' => '删除用户']);
+        Permission::create(['name' => 'user_index', 'title' => '查看用户']);
+        Permission::create(['name' => 'user_edit', 'title' => '修改用户']);
+        Permission::create(['name' => 'role_add', 'title' => '添加角色']);
+        Permission::create(['name' => 'role_del', 'title' => '删除角色']);
+        Permission::create(['name' => 'role_index', 'title' => '查看角色']);
+        Permission::create(['name' => 'role_edit', 'title' => '修改角色']);
+        Permission::create(['name' => 'permission_add', 'title' => '添加权限']);
+        Permission::create(['name' => 'permission_del', 'title' => '删除权限']);
+        Permission::create(['name' => 'permission_index', 'title' => '查看权限']);
+        Permission::create(['name' => 'permission_edit', 'title' => '修改权限']);
 
         // 创建站长角色，并赋予权限
         $founder = Role::create(['name' => 'Founder']);
-        $founder->givePermissionTo('manage_contents');
-        $founder->givePermissionTo('manage_users');
-        $founder->givePermissionTo('edit_settings');
+        $founder->givePermissionTo('post_del');
+        $founder->givePermissionTo('post_index');
+        $founder->givePermissionTo('reply_del');
+        $founder->givePermissionTo('reply_index');
+        $founder->givePermissionTo('user_add');
+        $founder->givePermissionTo('user_del');
+        $founder->givePermissionTo('user_index');
+        $founder->givePermissionTo('user_edit');
+        $founder->givePermissionTo('role_add');
+        $founder->givePermissionTo('role_del');
+        $founder->givePermissionTo('role_index');
+        $founder->givePermissionTo('role_edit');
+        $founder->givePermissionTo('permission_add');
+        $founder->givePermissionTo('permission_del');
+        $founder->givePermissionTo('permission_index');
+        $founder->givePermissionTo('permission_edit');
 
         // 创建管理员角色，并赋予权限
         $maintainer = Role::create(['name' => 'Maintainer']);
-        $maintainer->givePermissionTo('manage_contents');
+        $maintainer->givePermissionTo('post_del');
+        $maintainer->givePermissionTo('post_index');
+        $maintainer->givePermissionTo('reply_del');
+        $maintainer->givePermissionTo('reply_index');
+        $maintainer->givePermissionTo('user_index');
+        $maintainer->givePermissionTo('user_edit');
+
+        // 创建普通成员角色，并赋予权限
+        $member = Role::create(['name' => 'member']);
+        $member->givePermissionTo('post_del');
+        $member->givePermissionTo('post_index');
+        $member->givePermissionTo('reply_del');
+        $member->givePermissionTo('reply_index');
     }
 
     public function down()
